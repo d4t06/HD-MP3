4d05218cc0c38607da8d173d9d341656c4b252f2
datnh <huudat01234560@gmail.com>
2023-08-16 22:16:26 +0700

    update route for deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import Playlist from "../pages/Playlist";
 import Favorite from "../pages/Favorite";
 
 const publicRoutes = [
-   {path: '/', component: HomePage},
+   {path: '/React-Zingmp3', component: HomePage},
    {path: '/songs', component: SongsPage},
    {path: '/playlist', component: Playlist},
    {path: '/favorite', component: Favorite},
