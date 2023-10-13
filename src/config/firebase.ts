b8aca363c30b767f9f4b7106f2557bf62d6cafbf
datnh <huudat01234560@gmail.com>
2023-10-13 13:20:51 +0700

    update sang thu 6 13/10/2023

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,16 +2,9 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import firebaseConfig from "./firebaseConfig";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBPNpckpYiu009eSzHz_1YMlwHLtScgVjE",
-  authDomain: "zingmp3-clone-61799.firebaseapp.com",
-  projectId: "zingmp3-clone-61799",
-  storageBucket: "zingmp3-clone-61799.appspot.com",
-  messagingSenderId: "938280289868",
-  appId: "1:938280289868:web:1b426fc2dba5d03dde96e9"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
